3a3672136bb0fef6177a75fb5ffc8a072c5ab657
Vladislav Larkin <larkin.qubership@gmail.com>
2026-05-29 16:07:14 +0400

    chore: archive repository, move source code to qubership-core-java-libs

--- context-propagation/runtime/src/main/java/com/netcracker/cloud/context/propagation/quarkus/runtime/filter/Priorities.java
+++
@@ -1,6 +0,0 @@
-package com.netcracker.cloud.context.propagation.quarkus.runtime.filter;
-
-public class Priorities {
-    public static final int CORE_CONTEXT_PROPAGATION_OUTGOING_RESPONSE = jakarta.ws.rs.Priorities.AUTHENTICATION - 1000;
-    public static final int CORE_CONTEXT_PROPAGATION_INCOMING_REQUEST = jakarta.ws.rs.Priorities.AUTHORIZATION - 2;
-}
